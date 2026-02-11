aa529bf881895feb2c61fe74ba70c07ffe8eae5e
Diego Fernandez <diegofdz2004@gmail.com>
2026-02-11 14:03:45 -0500

    Added combat ui

---
+++ scenes/battle/mech-screen/energy-body.gd
@@ -0,0 +1,32 @@
+extends TextureRect
+
+func _ready():
+	display_health(randf())
+
+"""
+Change the healthbar to display current health.
+Health should be a variable from 0-1.
+"""
+func display_health(health):
+	if health > .9:
+		texture = load("res://assets/ui/energy/body/energy-body-100.png")
+	elif health > .8:
+		texture = load("res://assets/ui/energy/body/energy-body-90.png")
+	elif health > .7:
+		texture = load("res://assets/ui/energy/body/energy-body-80.png")
+	elif health > .6:
+		texture = load("res://assets/ui/energy/body/energy-body-70.png")
+	elif health > .5:
+		texture = load("res://assets/ui/energy/body/energy-body-60.png")
+	elif health > .4:
+		texture = load("res://assets/ui/energy/body/energy-body-50.png")
+	elif health > .3:
+		texture = load("res://assets/ui/energy/body/energy-body-40.png")
+	elif health > .2:
+		texture = load("res://assets/ui/energy/body/energy-body-30.png")
+	elif health > .1:
+		texture = load("res://assets/ui/energy/body/energy-body-20.png")
+	elif health > 0:
+		texture = load("res://assets/ui/energy/body/energy-body-10.png")
+	else:
+		texture = load("res://assets/ui/energy/body/energy-body-0.png")
